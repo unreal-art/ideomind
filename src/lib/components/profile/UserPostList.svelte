71a83007ad9375c42e34a0fad7ef05d78ab24db9
codypharm <williamikeji@gmail.com>
2024-12-09 14:47:40 +0100

    upload to pinata

--- src/lib/components/profile/UserPostList.svelte
+++ src/lib/components/profile/UserPostList.svelte
@@ -21,25 +21,26 @@
 
 <div class="  columns-1 justify-center gap-4 sm:columns-2 lg:columns-4">
 	{#each data as item}
-		<a href={`/details/${item.id}`}
-			><div class="relative mb-6 break-inside-avoid">
-				<div class="absolute bottom-1 right-0 flex items-center text-white">
-					<Button variant="ghost"><Ellipsis size={20}></Ellipsis></Button>
+		>
+		<div class="relative mb-6 break-inside-avoid">
+			<div class="absolute bottom-1 right-0 flex items-center text-white">
+				<Button variant="ghost"><Ellipsis size={20}></Ellipsis></Button>
 
-					<Button
-						variant="ghost"
-						onclick={() => like(item)}
-						class="flex space-x-2 hover:bg-black/50 hover:text-white"
-					>
-						<span>{item.likes}</span>
-						<Heart
-							size={20}
-							class={`${isLikes || isInLikedPosts(item.id) ? 'fill-pink-500 text-pink-500' : ''}`}
-						></Heart></Button
-					>
-				</div>
-				<img src={item.images[0]} alt="user profile" class="mb-6 w-full rounded-sm" />
+				<Button
+					variant="ghost"
+					onclick={() => like(item)}
+					class="flex space-x-2 hover:bg-black/50 hover:text-white"
+				>
+					<span>{item.likes}</span>
+					<Heart
+						size={20}
+						class={`${isLikes || isInLikedPosts(item.id) ? 'fill-pink-500 text-pink-500' : ''}`}
+					></Heart></Button
+				>
 			</div>
-		</a>
+			<a href={`/details/${item.id}`}
+				><img src={item.images[0]} alt="user profile" class="mb-6 w-full rounded-sm" /></a
+			>
+		</div>
 	{/each}
 </div>
