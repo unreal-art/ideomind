c4b8f4cbabb5064d84b50598a2475ef259335296
codypharm <williamikeji@gmail.com>
2024-12-22 21:53:52 +0100

    load post in first page

---
+++ src/routes/+layout.ts
@@ -0,0 +1,34 @@
+// Update the path to your Supabase client
+import type { User } from "@/types";
+import { supabase } from "../supabaseClient";
+import { authenticate } from "@/api";
+import { goto } from "$app/navigation";
+import type { LayoutLoad } from "./$types";
+import { store } from "$lib/store";
+
+export const load: LayoutLoad = async ({ url }) => {
+	
+
+	//fetch post data
+	try {
+		// Fetch posts with likes
+		const { data: posts, error } = await supabase.from("posts").select(`
+        *,
+        likes (
+          *
+        )
+      `).limit(10);
+
+		if (error) {
+			console.error("Error fetching posts with likes:", error);
+			return { posts: [] };
+		}
+
+		store.initPosts(posts);
+	} catch (err) {
+		console.error("Unexpected error:", err);
+		return { posts: [] };
+	}
+
+	
+};
