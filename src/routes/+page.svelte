aec44e9a3ffbc5407d8d733d507cf9ea18496405
codypharm <williamikeji@gmail.com>
2024-12-22 19:09:41 +0100

    redirect error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,7 @@
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: "github",
 			options: {
-				redirectTo: `http://localhost:5173/explore`
+				redirectTo: `https://unreal.art/explore`
 			}
 		});
 	}
