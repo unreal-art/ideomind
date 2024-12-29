f04b72e22daaecb145320631aa093aacd2691efa
Hiro <laciferin2024@protonmail.com>
2024-12-29 12:29:41 +0530

    hotfix: oauth issues (#41)
    * pretty
    
    * fixed: discord oauth
    
    * encodeURI:  signin with google
    
    * hotfixed: all the code
    
    * just logging:

--- src/routes/explore/+layout.ts
+++ src/routes/explore/+layout.ts
@@ -1,5 +1,4 @@
 // Update the path to your Supabase client
-import type { User } from "@/types";
 import { supabase } from "../../supabaseClient";
 import { authenticate } from "@/api";
 import { goto } from "$app/navigation";
@@ -12,53 +11,6 @@ export const load: LayoutLoad = async ({ url }) => {
 	// Get the full URL to pass to `exchangeCodeForSession`
 	const fullUrl = url.href;
 	console.log(`Loading ${fullUrl}`);
-
-	// Exchange the code for a session if the user is redirected back from Discord
-	if (url.searchParams.has("access_token") || url.searchParams.has("code")) {
-		const accessToken = url.searchParams.get("access_token") || url.searchParams.get("code");
-
-		// const { data, error } = await supabase.auth.exchangeCodeForSession(accessToken as string);
-		const { data, error } = await supabase.auth.exchangeCodeForSession(fullUrl);
-
-		if (error) {
-			console.error("Error during token exchange:", error.message);
-
-			await bluebird.delay(5 * 1000);
-			// return { user: null, error: error.message };
-
-			goto("/");
-		}
-
-		// Get the current user session after token exchange
-		const { data: sessionData } = await supabase.auth.getSession();
-		const userData = sessionData?.session?.user || null;
-		const { data: profileData } = await supabase
-			.from("profiles")
-			.select("*")
-			.eq("id", userData?.id);
-		if (!profileData) return;
-		const user = {
-			id: profileData[0].id,
-			name: profileData[0].full_name,
-			username: userData?.user_metadata.name,
-			email: userData?.email,
-			image: userData?.user_metadata.picture,
-
-			bio: profileData[0].bio,
-			followerCount: profileData[0].follower_count,
-			followingCount: profileData[0].following_count,
-			following: [],
-			location: profileData[0].location,
-			likesReceived: profileData[0].likesReceived,
-			creditBalance: profileData[0].credit_balance,
-			//@ts-ignore
-			createdAt: new Date(userData?.created_at),
-			//@ts-ignore
-			updatedAt: new Date(userData?.updated_at)
-		};
-
-		authenticate(user);
-	}
 
 	//fetch post data
 	try {
