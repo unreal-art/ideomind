46bc230c10f2c598f52f29c27772a6f930a2f34e
Nasfame <laciferin2024@protonmail.com>
2024-12-30 01:38:37 +0530

    code==0; outputFolder

--- src/routes/darts/+server.ts
+++ src/routes/darts/+server.ts
@@ -118,7 +118,8 @@ export const POST: RequestHandler = async ({ request }) => {
 		childProcess.on("close", async (code) => {
 			console.log(`child process exited with code ${code}`);
 			console.log(outputFolder);
-			if ((code === 0 || code === null) && outputFolder) {
+			// (code === 0 || code === null) && outputFolder;
+			if (outputFolder) {
 				//upload image + `/outputs/${formatText(jobDto.inputs?.Prompt as string)}`
 				try {
 					const uploadResponse = await uploadFilesInOutputs(outputFolder);
