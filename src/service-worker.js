25062f65a72a2bbf69d0ac80d700b76a4ff694c6
Nasfame <laciferin2024@protonmail.com>
2025-01-09 03:29:45 +0530

    sw : ready ; now app works offline. UX support to go.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,7 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
 import { build, files, version } from "$service-worker";
 
 // Create a unique cache name for this deployment
