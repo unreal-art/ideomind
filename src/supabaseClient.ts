38dfab763cda7256e21fc5d70b691f8dad5e7538
Nasfame <laciferin2024@protonmail.com>
2025-01-09 04:03:02 +0530

    revert: supabase client

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,10 +4,10 @@ import { createClient, type SupabaseClient } from "@supabase/supabase-js";
 const supabaseUrl = PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY;
 
-// export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
-export let supabase: SupabaseClient;
+// export let supabase: SupabaseClient;
 
-if (supabaseUrl) {
-	supabase = createClient(supabaseUrl, supabaseAnonKey);
-}
+// if (supabaseUrl) {
+// 	supabase = createClient(supabaseUrl, supabaseAnonKey);
+// }
