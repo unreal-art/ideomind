7c7f7e75da4189da84847ce2dfcd2de43666d36f
codypharm <williamikeji@gmail.com>
2025-01-09 01:00:44 +0100

    chore: code format

--- src/lib/components/detail/DetailList.svelte
+++ src/lib/components/detail/DetailList.svelte
@@ -4,21 +4,22 @@
 	import Image from "../Image.svelte";
 	import More from "../More.svelte";
 	import Likes from "@/Likes.svelte";
-	let readyBlocks = $state<string[]>([])
+	let readyBlocks = $state<string[]>([]);
 
 	// let likedPosts = $derived(getUserLikedPosts($store.user?.id));
 	let { data }: { data: Post[] } = $props();
 
-	function onReadyEvent(data: string){
-		readyBlocks.push(data)
+	function onReadyEvent(data: string) {
+		readyBlocks.push(data);
 	}
 </script>
 
-<div class=" grid  grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 pb-6">
+<div
+	class=" grid grid-cols-1 justify-center gap-4 pb-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+>
 	{#each data as post}
 		<div class="mb-6 break-inside-avoid">
-			
-			<Image item={post} {onReadyEvent}/>
+			<Image item={post} {onReadyEvent} />
 			{#if readyBlocks.includes(post.id)}
 				<div class="mt-3 flex h-10 w-full items-center justify-end">
 					<More />
--- src/lib/components/explore/PostList.svelte
+++ src/lib/components/explore/PostList.svelte
@@ -9,39 +9,39 @@
 	import PostAuthor from "../PostAuthor.svelte";
 	import Likes from "@/Likes.svelte";
 
-	let readyBlocks = $state<string[]>([])
+	let readyBlocks = $state<string[]>([]);
 
 	let { data }: { data: Post[] } = $props();
-	function onReadyEvent(data: string){
-		readyBlocks.push(data)
+	function onReadyEvent(data: string) {
+		readyBlocks.push(data);
 	}
-
 </script>
 
-<div class=" grid  grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 pb-6 pt-14 lg:pt-4">
+<div
+	class=" grid grid-cols-1 justify-center gap-4 pb-6 pt-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:pt-4 xl:grid-cols-5"
+>
 	{#each data as item}
-	<div class="mb-6 break-inside-avoid">
-		<Image {item} {onReadyEvent}/>
-		{#if readyBlocks.includes(item.id)}
-			<div class="mt-3 flex h-10 w-full items-center justify-between">
-				<a href={`/profile/${item.author}`}>
-				<div class="flex h-full space-x-2">
-					<PosterImage authorId={item.author} />
-					<div class="flex flex-col">
-						<PostAuthor authorId={item.author} />
-						<p class="text-light text-sm text-gray-400">
-							{item.createdAt && formatDistanceToNow(item.createdAt)} ago
-						</p>
+		<div class="mb-6 break-inside-avoid">
+			<Image {item} {onReadyEvent} />
+			{#if readyBlocks.includes(item.id)}
+				<div class="mt-3 flex h-10 w-full items-center justify-between">
+					<a href={`/profile/${item.author}`}>
+						<div class="flex h-full space-x-2">
+							<PosterImage authorId={item.author} />
+							<div class="flex flex-col">
+								<PostAuthor authorId={item.author} />
+								<p class="text-light text-sm text-gray-400">
+									{item.createdAt && formatDistanceToNow(item.createdAt)} ago
+								</p>
+							</div>
+						</div>
+					</a>
+					<div class="flex items-center">
+						<More />
+						<Likes id={item.id} post={item} />
 					</div>
 				</div>
-			</a>
-				<div class="flex items-center">
-					<More />
-					<Likes id={item.id} post={item} />
-				</div>
-			</div>
-				{/if}
+			{/if}
 		</div>
-	
 	{/each}
 </div>
